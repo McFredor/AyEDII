8cd229c1cdb33f35b6d2f59a53637a97564342a3
McFredor <wilfredoavilaok@gmail.com>
2023-03-22 02:22:44 -0300

    fin lab00

--- lab00/tictactoe.c
+++ lab00/tictactoe.c
@@ -4,7 +4,12 @@
 
 #include <assert.h>  /* assert() */
 
-#define CELL_MAX (3 * 3 - 1)
+/*  This is the variable that is used in exercise 3.
+    To have point a) change the 3 to a 4.
+    To have point b) change the 3 to a 5.    */
+#define BOARD_LONG 3 
+
+#define CELL_MAX (BOARD_LONG * BOARD_LONG - 1)
 
 void print_sep(int length) {
     printf("\t ");
@@ -13,28 +18,28 @@ void print_sep(int length) {
 
 }
 
-void print_board(char board[3][3])
+void print_board(char board[BOARD_LONG][BOARD_LONG])
 {
     int cell = 0;
 
-    print_sep(3);
-    for (int row = 0; row < 3; ++row) {
-        for (int column = 0; column < 3; ++column) {
+    print_sep(BOARD_LONG);
+    for (int row = 0; row < BOARD_LONG; ++row) {
+        for (int column = 0; column < BOARD_LONG; ++column) {
             printf("\t | %d: %c ", cell, board[row][column]);
             ++cell;
         }
         printf("\t | \n");
-        print_sep(3);
+        print_sep(BOARD_LONG);
     }
 }
 
-char get_line_winner(char l[3], char prev_winner)
+char get_line_winner(char l[BOARD_LONG], char prev_winner)
 {
     char new_winner = prev_winner;
     bool exist_serie = false;
     if(l[0] != '-'){
         exist_serie = true;
-        for(int i=1; i<3; i++){
+        for(int i=1; i<BOARD_LONG; i++){
             if(l[i-1] != l[i]){
                 exist_serie = false;
             }
@@ -50,32 +55,32 @@ char get_line_winner(char l[3], char prev_winner)
     return new_winner;
 }
 
-char get_winner(char board[3][3])
+char get_winner(char board[BOARD_LONG][BOARD_LONG])
 {
     char winner = '-';
-    char line[3];
+    char line[BOARD_LONG];
 
-    for(int i=0; i<3; i++) {
+    for(int i=0; i<BOARD_LONG; i++) {
         //horizontal
-        for(int j=0; j<3; j++) {
+        for(int j=0; j<BOARD_LONG; j++) {
             line[j]= board[i][j];
         }
         winner = get_line_winner(line, winner);
         
         //vertical
-        for(int j=0; j<3; j++) {
+        for(int j=0; j<BOARD_LONG; j++) {
             line[j]= board[j][i];
         }
         winner = get_line_winner(line, winner);
     }
 
     //diagonals
-    for(int i=0; i<3; i++) {
+    for(int i=0; i<BOARD_LONG; i++) {
         line[i]= board[i][i];
     }
     winner = get_line_winner(line, winner);
-    for(int i=0; i<3; i++) {
-        int j = (3-1)-i;
+    for(int i=0; i<BOARD_LONG; i++) {
+        int j = (BOARD_LONG-1)-i;
         line[i]= board[i][j];
     }
     winner = get_line_winner(line, winner);
@@ -83,11 +88,11 @@ char get_winner(char board[3][3])
     return winner;
 }
 
-bool has_free_cell(char board[3][3])
+bool has_free_cell(char board[BOARD_LONG][BOARD_LONG])
 {
     bool free_cell=false;
-    for (int i=0; i<3; i++) {
-        for (int j=0; j<3; j++) {
+    for (int i=0; i<BOARD_LONG; i++) {
+        for (int j=0; j<BOARD_LONG; j++) {
             if(board[i][j] == '-')
             free_cell=true;
             break;
@@ -100,11 +105,12 @@ int main(void)
 {
     printf("TicTacToe\n");
 
-    char board[3][3] = {
-        { '-', '-', '-' },
-        { '-', '-', '-' },
-        { '-', '-', '-' }
-    };
+    char board[BOARD_LONG][BOARD_LONG];
+    for(int i=0; i<BOARD_LONG; i++){
+        for(int j=0; j<BOARD_LONG; j++){
+            board[i][j] = '-';
+        }
+    }
 
     char turn = 'X';
     char winner = '-';
@@ -119,8 +125,8 @@ int main(void)
             exit(EXIT_FAILURE);
         }
         if (cell >= 0 && cell <= CELL_MAX) {
-            int row = cell / 3;
-            int colum = cell % 3;
+            int row = cell / BOARD_LONG;
+            int colum = cell % BOARD_LONG;
             if (board[row][colum] == '-') {
                 board[row][colum] = turn;
                 turn = turn == 'X' ? 'O' : 'X';
